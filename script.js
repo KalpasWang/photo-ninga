ae8dc04bed0a0d18ab04d5b74cbe8e266c189393
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-03-12 23:29:33 +0800

    add scrollspy and tooltip

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
   $('.tabs').tabs();
   $('.datepicker').datepicker({
     disableWeekends: true,
-    yearRange: 10
+    yearRange: 1
   });
-
+  var elems = document.querySelectorAll('.scrollspy');
+  var instances = M.ScrollSpy.init(elems, {throttle: 5000});
+  $('.tooltipped').tooltip();
 });
