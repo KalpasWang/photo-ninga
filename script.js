03ae128908588ab8df2dd6904b779629040ded7a
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-03-15 22:53:44 +0800

    add back to top scrollspy and refactoring

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-  $('.materialboxed').materialbox();
-  $('.parallax').parallax();
-  $('.tabs').tabs();
-  $('.datepicker').datepicker({
+// $(document).ready(function(){
+//   $('.sidenav').sidenav();
+//   $('.materialboxed').materialbox();
+//   $('.parallax').parallax();
+//   $('.tabs').tabs();
+//   $('.datepicker').datepicker({
+//     disableWeekends: true,
+//     yearRange: 1
+//   });
+//   var elems = document.querySelectorAll('.scrollspy');
+//   M.ScrollSpy.init(elems);
+//   $('.tooltipped').tooltip();
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems);
+  elems = document.querySelectorAll('.materialboxed');
+  M.Materialbox.init(elems);
+  elems = document.querySelectorAll('.parallax');
+  M.Parallax.init(elems);
+  elems = document.querySelectorAll('.tabs');
+  M.Tabs.init(elems);
+  elems = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(elems, {
     disableWeekends: true,
     yearRange: 1
   });
-  var elems = document.querySelectorAll('.scrollspy');
-  var instances = M.ScrollSpy.init(elems, {throttle: 5000});
-  $('.tooltipped').tooltip();
+  elems = document.querySelectorAll('.scrollspy');
+  M.ScrollSpy.init(elems);
+  elems = document.querySelectorAll('.tooltipped');
+  M.Tooltip.init(elems);
 });
